fd2deaf1af26e1f56478b8fc5b94f517ad2b30b3
Sumit Dube <sumitdube2303@gmail.com>
2024-08-29 22:08:24 +0530

    01_variables.js added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 10;
+let accountName = "Sumit";
+var accountPass = "Donkeykong";
+let accountNo;
+
+// accountId = 134; const cannot be defined again.
+
+accountName = "Ramesh";
+accountState = "Maharashtra"; // accepted state
+
+console.log(accountId);
+console.table([accountId, accountName, accountPass, accountState, accountNo]);
