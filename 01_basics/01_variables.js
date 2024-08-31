e14c05bcc417718eafe70666ff0342271f220155
sumkun <sumitdube2303@gmail.com>
2024-08-31 22:30:31 +0530

    02_datatypes.js added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,11 @@ let accountNo;
 
 // accountId = 134; const cannot be defined again.
 
+/*
+Prefer not to user var 
+because of issue in functional and block scope  
+*/
+
 accountName = "Ramesh";
 accountState = "Maharashtra"; // accepted state
 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+let name = "sumit" //string
+const id = 10 //numbers
+let isPassed = true //boolean
+let array = [10, 122, "dhoom"] //object
+let arrayObject = {
+    "name":"basshabhai", "id":10
+} //object
+let arrayis = null; //null is object 
+let state; //undefined datatype is undefined
+
+console.table([typeof state, typeof name, typeof id, typeof isPassed, typeof array, typeof arrayObject, typeof arrayis]);
+
+/*
+Types of Data types in JS:
+1. String => ""
+2. numbers => 2 to power of 53
+3. bigInt
+4. boolean => true/false
+5. null => standalone value
+6. Undefined => Undefined
+7. symbol => unique
+*/
